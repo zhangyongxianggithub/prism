03fdbec9c2e7b32a1302748e92cd4a438726d8ba
zhangyongxiang <1306835795@qq.com>
2025-04-20 18:03:25 +0800

    update

---
+++ src/main/java/com/bestzyx/prism/utils/CharPool.java
@@ -0,0 +1,11 @@
+package com.bestzyx.prism.utils;
+
+/**
+ * Created by zhangyongxiang on 2025/4/20 18:01
+ *
+ * @author zhangyongxiang
+ */
+public final class CharPool {
+    
+    private CharPool() {}
+}
