f53a69b316553fe744fcc03fe60216aec73b6d7f
zhangyongxiang <1306835795@qq.com>
2025-04-22 20:16:54 +0800

    update

--- src/main/java/com/bestzyx/prism/utils/CharPool.java
+++ src/main/java/com/bestzyx/prism/utils/CharPool.java
@@ -9,5 +9,71 @@ public final class CharPool {
     
     private CharPool() {}
     
+    public static char AT = '@';
+    
+    public static char POUND = '#';
+    
+    public static char DOLLAR = '$';
+    
+    public static char PERCENT = '%';
+    
+    public static char CARET = '^';
+    
+    public static char AMPERSAND = '&';
+    
+    public static char ASTERISK = '*';
+    
+    public static char TILDE = '~';
+    
+    public static char LEFT_PARENTHESES = '(';
+    
+    public static char RIGHT_PARENTHESES = ')';
+    
+    public static char LEFT_SQUARE_BRACKETS = '[';
+    
+    public static char RIGHT_SQUARE_BRACKETS = ']';
+    
+    public static char LEFT_CURLY_BRACKETS = '{';
+    
+    public static char RIGHT_CURLY_BRACKETS = '}';
+    
+    public static char LEFT_ANGLE_BRACKETS = '<';
+    
+    public static char RIGHT_ANGLE_BRACKETS = '>';
+    
+    public static char FORWARD_SLASH = '/';
+    
+    public static char EXCLAMATION_MARK = '!';
+    
+    public static char QUESTION_MARK = '?';
+    
+    public static char COLON = ':';
+    
+    public static char SEMICOLON = ';';
+    
+    public static char COMMA = ',';
+    
+    public static char DOT = '.';
+    
+    public static char DOUBLE_QUOTE = '"';
+    
+    public static char SINGLE_QUOTE = '\'';
+    
+    public static char UNDERSCORE = '_';
+    
     public static char DASH = '-';
+    
+    public static char EQUALS = '=';
+    
+    public static char PLUS = '+';
+    
+    public static char MINUS = '-';
+    
+    public static char HYTHEN = '-';
+    
+    public static char SLASH = '/';
+    
+    public static char BACKSLASH = '\\';
+    
+    public static char BACKTICK = '`';
 }
--- src/main/java/com/bestzyx/prism/utils/StrPool.java
+++ src/main/java/com/bestzyx/prism/utils/StrPool.java
@@ -6,8 +6,60 @@ package com.bestzyx.prism.utils;
  * @author zhangyongxiang
  */
 public final class StrPool {
-
+    
     private StrPool() {}
+    
+    public static String AT = "@";
+    
+    public static String POUND = "#";
+    
+    public static String DOLLAR = "$";
+    
+    public static String PERCENT = "%";
+    
+    public static String CARET = "^";
+    
+    public static String AMPERSAND = "&";
+    
+    public static String ASTERISK = "*";
+    
+    public static String TILDE = "~";
+    
+    public static String LEFT_PARENTHESES = "(";
+    
+    public static String RIGHT_PARENTHESES = ")";
+    
+    public static String LEFT_SQUARE_BRACKETS = "[";
+    
+    public static String RIGHT_SQUARE_BRACKETS = "]";
+    
+    public static String LEFT_CURLY_BRACKETS = "{";
+    
+    public static String RIGHT_CURLY_BRACKETS = "}";
+    
+    public static String LEFT_ANGLE_BRACKETS = "<";
+    
+    public static String RIGHT_ANGLE_BRACKETS = ">";
+    
+    public static String FORWARD_SLASH = "/";
+    
+    public static String EXCLAMATION_MARK = "!";
+    
+    public static String QUESTION_MARK = "?";
+    
+    public static String COLON = ":";
+    
+    public static String SEMICOLON = ";";
+    
+    public static String COMMA = ",";
+    
+    public static String DOT = ".";
+    
+    public static String DOUBLE_QUOTE = "\"";
+    
+    public static String SINGLE_QUOTE = "'";
+    
+    public static String UNDERSCORE = "_";
     
     public static String DASH = "-";
     
@@ -24,6 +76,4 @@ public final class StrPool {
     public static String BACKSLASH = "\\";
     
     public static String BACKTICK = "`";
-
-    public static String SINGLE_QUOTE = "'";
 }
