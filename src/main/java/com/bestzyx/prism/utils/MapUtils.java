af49317c0227ecfbe0336171617a85010a0da767
zhangyongxiang <1306835795@qq.com>
2025-05-15 16:31:03 +0800

    feat(utils): 添加 MapUtils 工具类
    - 新增 MapUtils 类，提供映射相关工具方法
    - 更新项目版本号至1.0.0.3

---
+++ src/main/java/com/bestzyx/prism/utils/MapUtils.java
@@ -0,0 +1,11 @@
+package com.bestzyx.prism.utils;
+
+/**
+ * Created by zhangyongxiang on 2025/5/15 14:49
+ *
+ * @author zhangyongxiang
+ */
+public final class MapUtils {
+    private MapUtils() {
+    }
+}
