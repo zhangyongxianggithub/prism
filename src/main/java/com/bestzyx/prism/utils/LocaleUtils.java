1703859b0ff159d0cc61c405244d5cf0993c0bcb
zhangyongxiang <1306835795@qq.com>
2025-05-15 13:42:09 +0800

    update

--- src/main/java/com/bestzyx/prism/utils/LocaleUtils.java
+++ src/main/java/com/bestzyx/prism/utils/LocaleUtils.java
@@ -1,10 +1,30 @@
 package com.bestzyx.prism.utils;
 
+import java.util.Locale;
+
 /**
  * Created by zhangyongxiang on 2025/4/20 18:01
  *
  * @author zhangyongxiang
  */
 public final class LocaleUtils {
-
+    
+    private LocaleUtils() {}
+    
+    public static String getLanguage(final Locale locale) {
+        return null;
+    }
+    
+    public static Locale getLocale(final String locale) {
+        final String[] split = locale.split("[_\\-]", 2);
+        if (split.length >= 2) {
+            return new Locale(split[0], split[1]);
+        } else {
+            return new Locale(split[0], "");
+        }
+    }
+    
+    public static Locale getLocale(final String language, final String region) {
+        return new Locale(language, region);
+    }
 }
---
+++ src/main/java/com/bestzyx/prism/utils/Region.java
@@ -0,0 +1,9 @@
+package com.bestzyx.prism.utils;
+
+/**
+ * Created by zhangyongxiang on 2025/5/15 13:29
+ *
+ * @author zhangyongxiang
+ */
+public enum Region {
+}
